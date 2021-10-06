89d116de81abb99cba9ebcc1307a941118349a6a
smk762 <35845239+smk762@users.noreply.github.com>
2021-10-06 18:05:44 +0800

    Update update_mm2.sh

--- update_mm2.sh
+++ update_mm2.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 rm mm2
-wget https://github.com/KomodoPlatform/atomicDEX-API/releases/download/beta-2.1.3690/mm2-1512af867-Linux-Debug.zip -O mm2-Linux-Debug.zip
-unzip mm2-Linux-Debug.zip
+wget https://github.com/KomodoPlatform/atomicDEX-API/releases/download/beta-2.1.4401/mm2-87837cb54-Linux-Release.zip -O mm2.zip
+unzip mm2.zip
+rm mm2.zip
