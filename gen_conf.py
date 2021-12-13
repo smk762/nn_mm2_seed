8ab37f461710c526f22b2b9fece11a189df72d2e
smk762 <35845239+smk762@users.noreply.github.com>
2021-12-14 07:14:39 +0800

    Update gen_conf.py

--- gen_conf.py
+++ gen_conf.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 import json
+import string
 import random
 import os.path
+import mnemonic
 
 conf = {
     "gui": "NN_SEED",
@@ -12,16 +14,49 @@ conf = {
     "userhome": "/${HOME#\"/\"}"
 }
 
+special_chars = ["$", "@", "~", "-", "_", "|", "(", ")", ":", "+"]
+
+def generate_rpc_pass(length):
+	rpc_pass = ""
+	quart = int(length/4)
+	while len(rpc_pass) < length:
+		rpc_pass += ''.join(random.sample(string.ascii_lowercase, random.randint(1,quart)))
+		rpc_pass += ''.join(random.sample(string.ascii_uppercase, random.randint(1,quart)))
+		rpc_pass += ''.join(random.sample(string.digits, random.randint(1,quart)))
+		rpc_pass += ''.join(random.sample(special_chars, random.randint(1,quart)))
+	str_list = list(rpc_pass)
+	random.shuffle(str_list)
+	return ''.join(str_list)
+	 
+resp = "Y"
 if os.path.exists("MM2.json"):
-	print("You already have an MM2.json file, move or delete it first.")
-else:
-	rpc_password = '%036x' % random.randrange(16**36)
+	resp = input("You already have an MM2.json file! Generate a new one? [Y/N]: ")
+	while resp not in ["Y", "y", "N", "n"]:
+		print("Invalid input!")
+		resp = input("You already have an MM2.json file! Generate a new one? [Y/N]: ")
+
+if resp in ["Y", "y"]:
+	rpc_password = generate_rpc_pass(16)
 	conf.update({"rpc_password": rpc_password})
-	passphrase = input("Enter a seed phrase for your seed node: ")
+
+	new_seed = input("[E]nter seed manually or [G]enerate one? [E/G]: ")
+	while new_seed not in ["G", "g", "E", "e"]:
+		print("Invalid input!")
+		new_seed = input("[E]nter seed manually or [G]enerate one? [E/G]: ")
+
+	if new_seed in ["E", "e"]:
+		passphrase = input("Enter a seed phrase: ")
+	else:		
+		m = mnemonic.Mnemonic('english')
+		passphrase = m.generate(strength=256)
+
 	conf.update({"passphrase": passphrase})
+
 	with open("MM2.json", "w+") as f:
 		json.dump(conf, f, indent=4)
+
 	print("MM2.json file created.")
+
 	with open("userpass", "w+") as f:
-		f.write(f"userpass={rpc_password}")
+		f.write(f'userpass="{rpc_password}"')
 	print("userpass file created.")
