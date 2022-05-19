f3e33b49a4fa0ea2ccbdbb96b833dfcda089b976
smk762 <35845239+smk762@users.noreply.github.com>
2022-05-19 18:00:53 +0800

    update netid for testnet

--- gen_conf.py
+++ gen_conf.py
@@ -7,7 +7,7 @@ import mnemonic
 
 conf = {
     "gui": "NN_SEED",
-    "netid": 7777,
+    "netid": 2022,
     "i_am_seed":True,
     "rpc_password": "RPC_CONTROL_USERPASSWORD",
     "passphrase": "YOUR SEED PHRASE",
