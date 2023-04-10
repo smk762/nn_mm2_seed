67303d9d35a174b30768d50993d0acc9d690fba8
smk762 <smk762@iinet.net.au>
2023-04-11 00:31:04 +0800

    use latest release url

--- update_mm2.sh
+++ update_mm2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 rm mm2
-#wget $(curl -vvv https://api.github.com/repos/KomodoPlatform/atomicDEX-API/releases | jq -r '.[0].assets | map(select(.name | contains("Linux-Release."))) | .[0].browser_download_url') -O mm2.zip
-wget http://195.201.0.6/dev/mm2-4366141c8-Linux-Release.zip -O mm2.zip
+wget $(curl -vvv https://api.github.com/repos/KomodoPlatform/atomicDEX-API/releases | jq -r '.[0].assets | map(select(.name | contains("Linux-Release."))) | .[0].browser_download_url') -O mm2.zip
+#wget http://195.201.0.6/dev/mm2-4366141c8-Linux-Release.zip -O mm2.zip
 unzip mm2.zip
 rm mm2.zip
